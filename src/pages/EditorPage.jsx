925a17e2e6cfe6d50d9ef772575688c2be85a72a
Bharat Rav <bharatrav8077@gmail.com>
2024-01-07 22:26:25 +0530

    code mirror page created codemirror setup remaining

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import jpgLogo from "../assets/logo.jpg";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
 const EditorPage = () => {
-  return <div>EditorPage is here</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Bharat Rav" },
+    { socketId: 2, username: "John Doe" },
+  ]);
+  return (
+    <div className="main-wrap">
+      <div className="aside">
+        <div className="aside-inner">
+          <div className="logo">
+            <img
+              src={jpgLogo}
+              width="388px"
+              height="180px"
+              className="logo-image"
+              alt="code-ed-logo"
+            />
+          </div>
+          <h3>Connected</h3>
+          <div className="clients-list">
+            {clients.map((client) => {
+              return (
+                <Client key={client.socketId} username={client.username} />
+              );
+            })}
+          </div>
+        </div>
+        <button className="btn copy-btn">Copy ROOM ID</button>
+        <button className="btn leave-btn">Leave</button>
+      </div>
+      <div className="editor-wrap">
+        <Editor />
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default EditorPage;
